2c675cc6988436da105e5cd8b354955382f54cbb
Voss <alexpeterson91@gmail.com>
2022-05-03 17:23:01 -0500

    Edit keygen name, and warning

--- src/react/pages/Home.tsx
+++ src/react/pages/Home.tsx
@@ -129,9 +129,9 @@ const Home: FC<HomeProps> = (props): ReactElement => {
         </div>
       </Modal>
 
-      <LandingHeader variant="h1">Welcome! <b>THIS IS EXPERIMENTAL SOFTWARE NOT INTENDED FOR PRODUCTION USE AT THIS TIME</b></LandingHeader>
+      <LandingHeader variant="h1">Welcome! <b>THIS IS ALPHA SOFTWARE NOT INTENDED FOR PRODUCTION USE AT THIS TIME</b></LandingHeader>
       <KeyIcon />
-      <SubHeader>Your key generator for Ethereum 2.0 & Gnosis Beacon Chain</SubHeader>
+      <SubHeader variant="h2">Your key generator for Gnosis Beacon Chain</SubHeader>
 
       <Links>
         <InfoLabel>Github:</InfoLabel> https://github.com/alexpeterson91/wagyu-key-gen/issues
@@ -150,7 +150,7 @@ const Home: FC<HomeProps> = (props): ReactElement => {
         </Grid>
         <Grid item>
           <Tooltip title={tooltips.IMPORT_MNEMONIC}>
-            <Button style={{color: "gray"}} size="small" onClick={handleUseExistingMnemonic} tabIndex={tabIndex(1)}>
+            <Button variant="contained" color="primary" onClick={handleUseExistingMnemonic} tabIndex={tabIndex(1)}>
               Use Existing Mnemonic Recovery Phrase
             </Button>
           </Tooltip>
